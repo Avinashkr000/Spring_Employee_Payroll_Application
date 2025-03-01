79a5bd2396a3e29315bed10f38bfd83f60106835
Avinashkr000 <akum97543@gmail.com>
2025-03-01 10:15:51 +0530

    [Avinash kumar] Add.  Ability to retrieve all the records of employee payroll pertaining to sales department

--- src/main/java/com/bridgelabz/employeepayrollapplication/repository/EmployeeRepository.java
+++ src/main/java/com/bridgelabz/employeepayrollapplication/repository/EmployeeRepository.java
@@ -2,8 +2,14 @@ package com.bridgelabz.employeepayrollapplication.repository;
 
 import com.bridgelabz.employeepayrollapplication.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    // UC6: Custom query to retrieve employees by department
+    @Query("SELECT e FROM Employee e WHERE :dept MEMBER OF e.department")
+    List<Employee> findEmployeesByDepartment(String dept);
 }
